9c55d773ebfada802298f574983ff2c0237712e7
Mykhailo <dzhammykhailo@knu.ua>
2025-04-11 02:42:10 +0300

    Домашка 11.1

--- HWOOP11_1/t11_7_a.py
+++ HWOOP11_1/t11_7_a.py
@@ -1,11 +1,34 @@
 def a_task(x, k):
     x_k = x
+    for i in range(1, k + 1):
+        x_k *= x ** 2 / (2 * i * (2 * i + 1))
+        yield x_k
+
+
+def b_task(x, k):
+    x_k = -x
+    for i in range(2, k + 1):
+        x_k *= -(x / i)
+        yield x_k
+
+def c_task(x, k):
+    x_k = 1
+    for i in range(2, k + 1):
+        x_k *= -x
+        for j in range(i**2-2*i, i**2-i+1):
+            if not j:
+                continue
+            x_k /= j
+        yield x_k
+
+def d_task(x, k):
+    x_k = 1
     for i in range(1, k+1):
-        x_k *= x **2 / (2 * i * (2 * i +1))
-    return x_k
+        x_k *= x / i
+        yield x_k * (i + 1)
 
 
-
-
-result = a_task(2, 2)
+result = d_task(2, 5)
+for x in result:
+    print(x)
 print(result)
