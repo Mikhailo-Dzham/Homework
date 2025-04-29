0021f5f2afe555963e8647051a77e23effd484c3
Mykhailo <dzhammykhailo@knu.ua>
2025-04-30 00:19:53 +0300

    Домашка 9 Якісь там вводи раціональних

--- HWOOP09/main.py
+++ HWOOP09/main.py
@@ -1,6 +1,15 @@
 from rational import Rational
 
-m = Rational(14)
+m = Rational("2/3")
 m1 = Rational(m)
 print(m.n, m.d)
 print(m1.n, m1.d)
+k = m1 * 3
+print(k.n, k.d)
+print(m())
+print(m["n"])
+x = 4  -  92  -  79  *  59  *  Rational ("90/16")  *  75  -  55  *  Rational("82/41")  *  19
+print(x["n"], x["d"])
+
+# with open("input.txt", 'r') as f:
+#     for line in
--- HWOOP09/rational.py
+++ HWOOP09/rational.py
@@ -14,13 +14,58 @@ class Rational:
             pass
         self.reduction()
 
+    def __add__(self, other):
+        oth = Rational(other)
+        return Rational(self.n * oth.d + self.d * oth.n, self.d * oth.d)
+
+    def __radd__(self, other):
+        oth = Rational(other)
+        return Rational(self.d * oth.n + self.n * oth.d, self.d * oth.d)
+
+    def __sub__(self, other):
+        oth = Rational(other)
+        return Rational(self.n * oth.d - self.d * oth.n, self.d * oth.d)
+
+    def __rsub__(self, other):
+        oth = Rational(other)
+        return Rational(self.d * oth.n - self.n * oth.d, self.d * oth.d)
+
+    def __truediv__(self, other):
+        oth = Rational(other)
+        return Rational(self.n * oth.d, self.d * oth.n)
+
+    def __rtruediv__(self, other):
+        oth = Rational(other)
+        return Rational(self.d * oth.n, self.n * oth.d)
+
+    def __mul__(self, other):
+        oth = Rational(other)
+        return Rational(self.n * oth.n, self.d * oth.d)
+
+    def __rmul__(self, other):
+        oth = Rational(other)
+        return Rational(self.n * oth.n, self.d * oth.d)
+
+    def __pow__(self, power, modulo=None):
+        return Rational(self.n ** power, self.d ** power)
+
+    def __call__(self):
+        return self.n / self.d
+
+    def __getitem__(self, item):
+        if item == 'd':
+            return self.d
+        else:
+            return self.n
+
     def reduction(self):
         a = max(self.n, self.d)
         b = self.n + self.d - a
         while a % b != 0:
             a, b = b, a % b
-        self. n //= b
+        self.n //= b
         self.d //= b
+
 
 class BadDenominator(Exception):
     pass
